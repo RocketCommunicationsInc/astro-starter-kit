808b17c7d8802decf4295e1a27244d3132be7678
Joel Perez <joel.perez@rocketcom.com>
2021-03-29 09:39:52 -0600

    added index css file

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,10 @@
-import globalStatusBar from "../../components/globalStatusBar";
+import globalStatusBar from "../../components/ruxGlobalStatusBar/ruxGlobalStatusBar";
 
 import "../../static/css/astro.css";
+import "./index.css";
 
 const astroApp = document.getElementById("astro-app");
 // Set theme by Class (dark-theme/light-theme)
 astroApp.classList.add("dark-theme");
 
-astroApp.appendChild(globalStatusBar());
+astroApp.insertBefore(globalStatusBar(), astroApp.firstChild);
